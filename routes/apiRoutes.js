fc9818fbd150a0e7464f2709fe2aa537f01bff3a
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-03-09 23:13:01 -0500

    creating post request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,14 +6,22 @@ const store = require('../db/store')
 
 router.get('/notes', (req, res) => {
     store
-    .getNotes()
-    .then((notes) => {
-        return res.json(notes);
-    })
-    .catch((err) => res.status(500).json(err));
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => res.status(500).json(err));
 })
 
 // create a post request
+router.post("/api/notes", (req, res) => {
+    const freshNote = req.body;
+    if (notes.length === 0) {
+        freshNote.id = 1
+    } else {
+        freshNote.id = (notes[notes.length - 1].id + 1);
+    }
+})
 
 // create a delete request
 
