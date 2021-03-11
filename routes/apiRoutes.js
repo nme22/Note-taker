033a34cf3b273bdcfa3fb41f71baeec4a4735690
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-03-10 23:36:13 -0500

    getting somewhere

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,28 +11,7 @@ module.exports = function (router) {
 
     // create a post request
     router.post("/api/notes/:id", (req, res) => {
-        fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
-            if (err) throw err;
-            const db = JSON.parse(data);
-            const newDB = [];
-
-            db.push(req.body);
-
-            for (let i = 0; i < db.length; i++) {
-                const newNote = {
-                    title: db[i].title,
-                    text: db[i].text,
-                    id: i
-                };
-
-                newDB.push(newNote);
-            }
-
-            fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
-                if (err) throw err;
-                res.json(req.body);
-            });
-        });
+        const id = req.params.id;
     });
 
     // create a delete request
